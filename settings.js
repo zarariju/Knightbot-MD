d883057bd185cd67aad0271b5ee8a8e6e06dc5a9
zarariju <riyazmohammadriya9@gmail.com>
2025-02-01 08:11:06 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
-  author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  packname: 'zoro',
+  author: 'zoro',
+  botName: "zoro efx",
+  botOwner: 'zoro', // Your name
+  ownerNumber: '917760274059', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
